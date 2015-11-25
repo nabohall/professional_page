d88637623750b1d2e18b78f12c58ed2a1dbc58fb
Nick Bohall <nabohall1224@icloud.com>
2015-11-24 19:40:01 -0800

    Finished for now

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # professional_page
-This is a webpage that will showcase not only my web dev projects, but also serve as my professional webpage, highlighting my math research, as well as my CV and resume
+This is a webpage that will showcase not only my web dev projects, but also serve as my professional webpage, highlighting my math research, as well as my CV and resume.
+
+The URL: http://students.washington.edu/nabohall/info343/professional_page
+
+Received help: no one.
+
+Hours to complete: ~12
+
+Challenges: I tried to use D3 layouts to manipulate the data, and that ended up being super challenging. I'm glad that I knew the basics, but I really wish I had known how to dynamically edit the data structures behind the layouts before I started this project... which ironically was discussed the day this was due. Also, I had wanted to attach anchors to my svgs in my angular directive, but found this hard as well, so it might be nice in future lectures to discuss how new concepts and libraries work, and how to merge previous information with new ideas to create a really interesting thing in class. 
---
+++ js/app.js
@@ -0,0 +1,171 @@
+//Make app
+angular.module("uiApp", ['ui.router', 'ui.bootstrap'])
+
+//Config UI and different pages
+.config(function($stateProvider){
+	$stateProvider
+		.state('home',{
+			url:'/',
+			templateUrl:'templates/home.html',
+			controller:'HomeController'
+		})
+		.state('web',{
+			url:'/web',
+			templateUrl:'templates/web.html',
+			controller:'WebController'
+		})
+		.state('research',{
+			url:'/research',
+			templateUrl:'templates/research.html',
+			controller:'ResearchController'
+		})
+		.state('contact',{
+			url:'/contact',
+			templateUrl:'templates/contact.html',
+			controller:'ContactController'
+		})
+		.state('documents',{
+			url:'/documents',
+			templateUrl:'templates/documents.html',
+			controller:'DocumentsController'
+		})
+})
+
+//UI controllers
+.controller('HomeController', function($scope,$http){
+
+})
+.controller('WebController', function($scope,$http){
+	$http.get('../data/web.json').then(function(response){
+		$scope.data = response.data;
+	})
+})
+.controller('ResearchController', function($scope,$http){
+	$http.get('../data/research.json').then(function(response){
+		$scope.data = response.data;
+	})
+})
+.controller('ContactController', function($scope,$http){
+	$http.get('../data/contact.json').then(function(response){
+		$scope.data = response.data;
+	})
+})
+.controller('DocumentsController', function($scope,$http){
+	$http.get('../data/documents.json').then(function(response){
+		$scope.data = response.data;
+	})
+})
+
+//D3 directive
+.directive('hexbin', function($filter,$compile) {
+
+	return {
+		restrict:'E',//Make a new HTML element
+
+		//give scope the data
+		scope:{
+			data:"=data"
+		},
+
+		//Create a link function that creates the hexbin element
+
+		link:function(scope,elem,attrs){
+			//Create a wrapper to the first element 
+			var wrapper = d3.select(elem[0])
+
+			//Height/width of the bin, where hexagons will lie
+			var height = 400,
+				width = 400,
+				circleValue = 100;
+
+			var svg = wrapper.append('svg')
+				.attr('width', width)
+				.attr('height', height);
+
+			var circleData = {
+				    children:[
+				    ]
+				}
+
+			//The layout
+			var layout;
+			//A scale
+			var colorScale = d3.scale.category20();
+			var defs;
+
+			//When data is assigned, assign it to the circle data and make patterns
+			scope.$watch('data', function(){
+
+				scope.data.forEach(function(d){
+					circleData.children.push(d);
+				})
+
+				defs = svg.append('defs')
+					.selectAll("pattern")
+					.data(scope.data)
+					.enter()
+					.append('pattern')
+					.attr('x', -100)
+					.attr('y',-100)
+				    .attr("width", 500)
+				    .attr("height", 500)
+				    .attr("id",function(d){return d.idName})
+				    .attr('patternUnits',"userSpaceOnUse")
+				    .append("image")
+				    .attr("width", 500)
+				    .attr("height", 500)
+				    .attr('x',30)
+				    .attr('y',50)
+				    .attr("xlink:href", function(d){return d.imgURL;});
+
+				makeLayout();
+				draw();
+			})
+
+			//Wait for data to be assigned to make the layout
+			var makeLayout = function(){
+				layout = d3.layout.pack()
+					.size([width,height])
+					.value(function(d){return circleValue;});
+			}
+			
+			//Reusable function to assign attributes to our circles
+			var bubbleFunc = function(circle){   	
+				 circle.attr('cx',function(d){return d.x;})
+				    .attr('cy', function(d){return d.y})
+				    .attr('r',function(d){return d.r})
+				    .attr('fill', function(d){return colorScale(d.title)})
+				    .attr('fill', function(d){return "url(#"+ d.idName +")";})
+				    .attr("tooltip-append-to-body", true)
+				    .attr('tooltip', function(d){
+				    	return d.title + ': ' + d.description;
+				    })
+
+			}
+
+			var draw = function(){
+				var circles = svg.selectAll('circle')
+					.data(layout.nodes(circleData))
+
+				circles.enter()
+					.append('circle')
+					.call(bubbleFunc)
+					.call(function(){
+						$compile(this[0].parentNode)(scope);
+					})
+
+				circles.exit()
+					.remove()
+
+				svg.selectAll('circle')
+					.call(bubbleFunc)
+
+			}
+		}
+	}
+})
+
+
+
+
+
